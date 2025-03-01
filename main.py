1ea298147613410ad91072cc099da867e28cd5cd
camunozv <93873199+camunozv@users.noreply.github.com>
2025-03-01 17:13:11 -0500

    PLY testing

--- main.py
+++ main.py
@@ -1,1 +1,123 @@
-print('HELLO NIGGAS!')
+# Am Anfang werden wir ein bischen von LEX und YACC lernen, damit
+# die generelle Grammatik der Kommandos defniert wird.
+# Naturlich habe ich keine fähigkeit mit solchen Werkzeugen. Deswegen 
+# werde ich ein bischen Praktizieren und dannach fange ich mit dem konkreten
+# Project an.
+
+# Das Project gliedert sich im sechs Teilen. 
+
+# 1. Das Begin: Wir machen keine Installation der Werkzeugen, weil sie support nicht mehr supportiert werden.
+
+from ply.lex import lex
+from ply.yacc import yacc
+
+# --- Tokenizer
+
+# All tokens must be named in advance.
+tokens = ( 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'LPAREN', 'RPAREN',
+           'NAME', 'NUMBER' )
+
+# Ignored characters
+t_ignore = ' \t'
+
+# Token matching rules are written as regexs
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+
+# A function can be used if there is an associated action.
+# Write the matching regex in the docstring.
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+# Ignored token with an action associated with it
+def t_ignore_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count('\n')
+
+# Error handler for illegal characters
+def t_error(t):
+    print(f'Illegal character {t.value[0]!r}')
+    t.lexer.skip(1)
+
+# Build the lexer object
+lexer = lex()
+
+## Bis hier funkioniert es
+
+# --- Parser
+
+# Write functions for each grammar rule which is
+# specified in the docstring.
+def p_expression(p):
+    '''
+    expression : term PLUS term
+               | term MINUS term
+    '''
+    # p is a sequence that represents rule contents.
+    #
+    # expression : term PLUS term
+    #   p[0]     : p[1] p[2] p[3]
+    # 
+    p[0] = ('binop', p[2], p[1], p[3])
+
+def p_expression_term(p):
+    '''
+    expression : term
+    '''
+    p[0] = p[1]
+
+def p_term(p):
+    '''
+    term : factor TIMES factor
+         | factor DIVIDE factor
+    '''
+    p[0] = ('binop', p[2], p[1], p[3])
+
+def p_term_factor(p):
+    '''
+    term : factor
+    '''
+    p[0] = p[1]
+
+def p_factor_number(p):
+    '''
+    factor : NUMBER
+    '''
+    p[0] = ('number', p[1])
+
+def p_factor_name(p):
+    '''
+    factor : NAME
+    '''
+    p[0] = ('name', p[1])
+
+def p_factor_unary(p):
+    '''
+    factor : PLUS factor
+           | MINUS factor
+    '''
+    p[0] = ('unary', p[1], p[2])
+
+def p_factor_grouped(p):
+    '''
+    factor : LPAREN expression RPAREN
+    '''
+    p[0] = ('grouped', p[2])
+
+def p_error(p):
+    print(f'Syntax error at {p.value!r}')
+
+# Build the parser
+parser = yacc()
+
+# Parse an expression
+# Um zu Arbeiten, müssen wir das Argument lexer geben.
+ast = parser.parse('2 * 3 + 4 * (5 - x)', lexer)
+print(ast)
